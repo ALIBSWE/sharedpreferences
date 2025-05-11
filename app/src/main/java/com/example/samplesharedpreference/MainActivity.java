3a5c6c9cf5bf90733b644c556471e6b63858130d
ALIBSWE <alibahari1222ab@gmail.com>
2025-05-11 12:39:30 +0330

    Update MainActivity.java

--- app/src/main/java/com/example/samplesharedpreference/MainActivity.java
+++ app/src/main/java/com/example/samplesharedpreference/MainActivity.java
@@ -17,14 +17,7 @@ public class MainActivity extends AppCompatActivity {
         userSettings = new UserSetting(this);
 
 
-//        below codes are for setting data in shared preferences and store that in android system in phone
-//        userSettings.setSharedPrefUser("hoseini");
-//        userSettings.setSharedPrefEmail("hoseini@gmail.com");
 
-
-//        below codes are for getting back data from shared preferences and they return String you can use that as string
-//        userSettings.getSharePrefEmail();
-//        userSettings.getSharedPrefUser();
 
         Toast.makeText(this, "email : " + userSettings.getSharePrefEmail() + " , User :" + userSettings.getSharedPrefUser(), Toast.LENGTH_SHORT).show();
 
